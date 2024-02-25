107a95cffb4978ba4159f11a8be712cd450671bb
Adrian Mayorga <adrian@mayorga.io>
2024-02-25 15:37:52 -0800

    read instructions from text file and send via ip

--- dotnet/Program.cs
+++ dotnet/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! - from Adrian");
+﻿using System.Net.Sockets;
+using System.Text;
+
+
+var lines = File.ReadAllLines("instructions.txt");
+if(lines.Length < 3 || !lines[0].StartsWith("IP") || !lines[1].StartsWith("PORT"))
+{
+    Console.WriteLine("Invalid file");
+    return;
+}
+string ipAddress = lines[0].Split(" ")[1];
+int port = Convert.ToInt32(lines[1].Split(" ")[1]);
+
+// Connect to the instrument
+TcpClient client = new TcpClient(ipAddress, port);
+NetworkStream stream = client.GetStream();
+
+foreach (var line in lines.Skip(2))
+{
+    SendCommand(stream, line);
+}
+
+
+// Close the connection
+stream.Close();
+client.Close();
+
+static void SendCommand(NetworkStream stream, string command)
+{
+    if (string.IsNullOrWhiteSpace(command))
+    {
+        return;
+    }
+    Console.WriteLine(command);
+    if (command.StartsWith("WAIT"))
+    {
+        int ms = Convert.ToInt32(command.Split(" ")[1]);
+        Thread.Sleep(ms);
+    }
+    else
+    {
+        // Convert the command to bytes and send it over the network stream
+        byte[] data = Encoding.ASCII.GetBytes(command + "\n");
+        stream.Write(data, 0, data.Length);
+    }
+}
