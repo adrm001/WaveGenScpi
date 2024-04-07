2b6de7c02a60b60c530deb0f44b17a07172fdaa5
Adrian Mayorga <adrian@mayorga.io>
2024-04-07 13:51:22 -0700

    Add def wait command

--- dotnet/Program.cs
+++ dotnet/Program.cs
@@ -11,31 +11,54 @@ if(lines.Length < 3 || !lines[0].StartsWith("IP") || !lines[1].StartsWith("PORT"
 string ipAddress = lines[0].Split(" ")[1];
 int port = Convert.ToInt32(lines[1].Split(" ")[1]);
 
+var waitMap = new Dictionary<string, int>();
+
 // Connect to the instrument
 TcpClient client = new TcpClient(ipAddress, port);
 NetworkStream stream = client.GetStream();
 
 foreach (var line in lines.Skip(2))
 {
-    SendCommand(stream, line);
+    SendCommand(stream, line, waitMap);
 }
-
 
 // Close the connection
 stream.Close();
 client.Close();
 
-static void SendCommand(NetworkStream stream, string command)
+static void SendCommand(NetworkStream stream, string command, Dictionary<string, int> waitMap)
 {
     if (string.IsNullOrWhiteSpace(command))
     {
         return;
     }
     Console.WriteLine(command);
-    if (command.StartsWith("WAIT"))
+    if (command.StartsWith("DEF"))
     {
-        int ms = Convert.ToInt32(command.Split(" ")[1]);
+        var parts = command.Split(" ");
+        if (parts.Length == 3 && parts[1].StartsWith("WAIT") && !"WAIT".Equals(parts[1]))
+        {            
+            int ms = Convert.ToInt32(parts[2]);
+            waitMap[parts[1]] = ms;
+        }
+    }
+    else if (command.StartsWith("WAIT"))
+    {
+        var parts = command.Split(" ");
+        if (!waitMap.TryGetValue(parts[0], out int ms))
+        {
+            if ("WAIT".Equals(parts[0]))
+            {
+                ms = Convert.ToInt32(parts[1]);
+            }
+            else
+            {
+                Console.WriteLine($"WARNING: wait not recognized - {parts[0]}");
+                return;
+            }            
+        }
         Thread.Sleep(ms);
+
     }
     else
     {
